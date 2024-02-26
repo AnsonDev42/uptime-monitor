419b078cd88805f7c9ce0e97ea22ae2c41bc2276
Anson <58594437+AnsonDev42@users.noreply.github.com>
2024-02-26 15:49:04 +0000

    feat: add bark notification

--- apps/notification/admin.py
+++ apps/notification/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import NotificationChannel, NotificationLog
 
-# Register your models here.
+admin.site.register(NotificationChannel)
+admin.site.register(NotificationLog)
--- apps/notification/models.py
+++ apps/notification/models.py
@@ -1,3 +1,4 @@
+import httpx
 from django.db import models
 from apps.service.models import Service
 
@@ -6,8 +7,32 @@ class NotificationChannel(models.Model):
     name = models.CharField(max_length=255, unique=True)
     details = models.JSONField()
 
+    class Meta:
+        abstract = True
+
     def __str__(self):
         return self.name
+
+    def send_notification(self, service, message):
+        raise NotImplementedError("Method not implemented")
+
+
+class BarkChannel(NotificationChannel):
+    endpoint = models.URLField(max_length=255, default="https://api.day.app")
+
+    def send_notification(self, service, message):
+        prepared_message = f"{service.name} - {message}"
+        # Send the notification to the bark server
+        # get the response and return it
+        with httpx.Client(http2=True) as client:
+            response = client.post(
+                f"{self.endpoint}/UptimeMonitor alert/{prepared_message}",
+                headers=self._headers,
+                json=message,
+            )
+            if not response.is_success:
+                return False
+            return True
 
 
 class NotificationLog(models.Model):
