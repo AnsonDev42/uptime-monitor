2023d093ff6071b3c3069844b6095c01f6e393dc
Anson <58594437+AnsonDev42@users.noreply.github.com>
2024-03-08 18:32:50 +0000

    test: fix test based on the service serializer

--- apps/service/tests.py
+++ apps/service/tests.py
@@ -4,7 +4,6 @@ from rest_framework.exceptions import ValidationError
 from apps.monitoring.serializers import PeriodicTaskSerializer
 from apps.notification.models import NotificationChannel, NotificationType
 from apps.service.serializers import ServiceSerializer
-from django_celery_beat.models import PeriodicTask, IntervalSchedule
 
 
 class ServiceSerializerValidationTest(TestCase):
@@ -35,45 +34,7 @@ class ServiceSerializerCreateTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         # Setup necessary data for tests, like creating PeriodicTasks and NotificationChannels
-        cls.periodic_task = PeriodicTask.objects.create(
-            name="Test Task1",
-            interval=IntervalSchedule.objects.create(
-                every=3, period=IntervalSchedule.DAYS
-            ),
-        )
-        cls.periodic_task.save()
-        cls.notification_channel = NotificationChannel.objects.create(
-            name="Test Channel",
-            type=NotificationType.BARK,
-            details={"url": "https://bark.example.com"},
-        )
-        ...
-
-    def test_create_service_with_periodic_task_id(self):
-        """Test service creation with a periodic_task_id."""
-        interval_schedule = IntervalSchedule.objects.create(every=3, period="days")
-        periodic_task = PeriodicTask.objects.create(
-            name="Test Task", interval=interval_schedule
-        )
-        data = {
-            "name": "brruno",
-            "description": "test",
-            "monitoring_endpoint": "http://localhost:8000/service/",
-            "is_active": True,
-            "notification_channel": [],
-            "monitoring_type": "http",
-            "periodic_task_id": periodic_task.id,
-            "periodic_task": None,
-        }
-        serializer = ServiceSerializer(data=data)
-        self.assertTrue(serializer.is_valid(), serializer.errors)
-        service = serializer.save()
-        self.assertEqual(service.periodic_task, periodic_task)
-
-    def test_create_service_with_periodic_task_data(self):
-        """Test service creation with periodic_task data."""
-        # periodic_task_serializer = PeriodicTaskSerializer(instance=self.periodic_task)
-        valid_data = {
+        cls.periodic_task_data = {
             "name": "Test Task",
             "task": "apps.monitoring.tasks.check_monitor_services_status",
             "kwargs": '{"service_id": 1}',
@@ -83,7 +44,18 @@ class ServiceSerializerCreateTest(TestCase):
             },
             "enabled": True,
         }
-        periodic_task_serializer = PeriodicTaskSerializer(data=valid_data)
+        cls.notification_channel = NotificationChannel.objects.create(
+            name="Test Channel",
+            type=NotificationType.BARK,
+            details={"url": "https://bark.example.com"},
+        )
+        cls.notification_channel.save()
+
+    def test_create_service_with_periodic_task_data(self):
+        """Test service creation with periodic_task data."""
+        # periodic_task_serializer = PeriodicTaskSerializer(instance=self.periodic_task)
+
+        periodic_task_serializer = PeriodicTaskSerializer(data=self.periodic_task_data)
         self.assertTrue(
             periodic_task_serializer.is_valid(), periodic_task_serializer.errors
         )
@@ -94,8 +66,7 @@ class ServiceSerializerCreateTest(TestCase):
             "is_active": True,
             "notification_channel": [],
             "monitoring_type": "http",
-            "periodic_task_id": None,
-            "periodic_task": periodic_task_serializer.data,
+            "periodic_task_data": self.periodic_task_data,
         }
         serializer = ServiceSerializer(data=data)
         self.assertTrue(serializer.is_valid(), serializer.errors)
@@ -103,4 +74,26 @@ class ServiceSerializerCreateTest(TestCase):
         self.assertIsNotNone(service.periodic_task)
         self.assertEqual(service.periodic_task.name, "Test Task")
 
-    # Add more tests for notification channel association, error cases, etc.
+    def test_create_service_with_notification_id(self):
+        """Test service creation with a_notification_id."""
+        data = {
+            "name": "brruno",
+            "description": "test",
+            "monitoring_endpoint": "http://localhost:8000/service/",
+            "is_active": True,
+            "notification_channel": [self.notification_channel.id],
+            "monitoring_type": "http",
+            "periodic_task_data": self.periodic_task_data,
+        }
+
+        self.assertTrue(
+            isinstance(self.notification_channel.id, int),
+            "Notification channel id is not an integer",
+        )
+        serializer = ServiceSerializer(data=data)
+        print(data)
+        self.assertTrue(serializer.is_valid(), serializer.errors)
+        service = serializer.save()
+        self.assertEqual(
+            service.notification_channel.first().id, self.notification_channel.id
+        )
